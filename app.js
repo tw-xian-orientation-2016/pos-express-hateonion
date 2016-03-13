3cc3299314369a77cfbba342abe3d13131aaedcf
Ouyang Yingjie <ouyang_yingjie@outlook.com>
2016-03-13 16:21:22 +0800

    feat:change receiptPage to swig template

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', list);
 app.use('/cart', cart);
-// app.use('/receipt', receipt);
+app.use('/receipt', receipt);
 // app.use('/receiptList', receiptList);
 
 
