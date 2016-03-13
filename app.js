728cf2fc25c2783c6256be510ae78cc6ec45b939
Ouyang Yingjie <ouyang_yingjie@outlook.com>
2016-03-13 14:54:58 +0800

    init:add some file from web-pos

---
+++ app.js
@@ -0,0 +1,69 @@
+"use strict";
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+
+let list = require('./routes/list');
+let cart = require('./routes/cart');
+let receipt = require('./routes/receipt');
+let receiptList = require('./routes/receiptList');
+
+let app = express();
+
+
+// view engine setup
+let swig = require('swig');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', list);
+app.use('/cart', cart);
+app.use('/receipt', receipt);
+app.use('/receiptList', receiptList);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ routes/list.js
@@ -0,0 +1,30 @@
+"use strict";
+let express = require('express');
+let router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('list', { items:[
+    {
+      id : "item001",
+      name : "苹果",
+      price : "3.00",
+      unit : "斤"
+    },
+    {
+      id : "item002",
+      name : "可乐",
+      price : "4.00",
+      unit : "瓶"
+    },
+    {
+      id : "item003",
+      name : "火腿肠",
+      price : "2.00",
+      unit : "根"
+    }
+  ] });
+});
+
+module.exports = router;
